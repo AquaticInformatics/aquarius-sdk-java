3a4490cf77f41e1ed6613923abd537f7ebb97849
aahuja <aahuja@AI-MOBILE-FIN08>
2025-11-03 17:12:10 -0800

    Updated-script

--- src/main/java/com/aquaticinformatics/aquarius/sdk/samples/create_service_models.sh
+++ src/main/java/com/aquaticinformatics/aquarius/sdk/samples/create_service_models.sh
@@ -24,6 +24,8 @@ Generator=../../../../../../../../../aquarius-sdk-net/src/SamplesServiceModelGen
 ServerName=$1
 OutputPath=$2
 
+command -v dotnet >/dev/null 2>&1 || exit_abort "This script requires the .NET 8 runtime. Grab it from here: https://www.microsoft.com/net/download"
+[-f "$GeneratorProjectPath"] || exit_abort "Can't find aquarius-sdk-net repo cloned to the same directory as aquarius-sdk-java"
 [ -f "$Generator" ]    || dotnet build "$GeneratorProjectPath" -c Release || exit_abort "Can't find or build SamplesServiceModelGenerator.dll"
 [ ! -z "$ServerName" ] || ServerName=https://demo.aqsamples.com
 [ ! -z "$OutputPath" ] || OutputPath=./ServiceModel.java
